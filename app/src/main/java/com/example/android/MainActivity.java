053af9f2f8dac78fb4a1003aebcff369cf42ce76
snigdha2402 <suramsnigdha@gmail.com>
2020-06-14 19:33:11 +0530

    commit ui

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.android;
-
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button nwuser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nwuser = (Button) findViewById(R.id.newuser);
+        nwuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newuser();
+            }
+        });
+    }
+    public void newuser(){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 }
