40670063faed9fef8a3131479be035123b6d05a0
snigdha2402 <suramsnigdha@gmail.com>
2020-06-16 20:52:38 +0530

    leave apply

---
+++ app/src/main/java/com/example/android/firstpg.java
@@ -0,0 +1,29 @@
+package com.example.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class firstpg extends AppCompatActivity {
+
+    Button nwlev;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstpg);
+        nwlev = (Button) findViewById(R.id.button2);
+        nwlev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newleave();
+            }
+        });
+    }
+    public void newleave(){
+        Intent intent = new Intent(this, leaveappl.class);
+        startActivity(intent);
+    }
+}
