78e8f6b3ab05ed401b1931e1706c219e3b0b98a9
snigdha2402 <suramsnigdha@gmail.com>
2020-06-17 19:46:36 +0530

    ui 2

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button nwuser;
+    Button login;
+    EditText eid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +21,20 @@ public class MainActivity extends AppCompatActivity {
                 newuser();
             }
         });
+     /*   login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });*/
     }
     public void newuser(){
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
     }
+  /*  public void login(){
+        Intent intent = new Intent(this,firstpg.class);
+        startActivity(intent);
+    } */
 }
--- app/src/main/java/com/example/android/firstpg.java
+++ app/src/main/java/com/example/android/firstpg.java
@@ -6,9 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class firstpg extends AppCompatActivity {
-
     Button nwlev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
