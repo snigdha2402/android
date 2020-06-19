dd85a780b13b71fc4bd3f41c9fbd16415df8c5c6
snigdha2402 <suramsnigdha@gmail.com>
2020-06-19 20:15:05 +0530

    ui 3

--- app/src/main/java/com/example/android/Main2Activity.java
+++ app/src/main/java/com/example/android/Main2Activity.java
@@ -3,12 +3,29 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Button register;
+    EditText empid,empname,deptno,desg,email,contact,address,pass,noleaves;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        empid=(EditText) findViewById(R.id.empno);
+        empname=(EditText) findViewById(R.id.empname);
+        deptno=(EditText) findViewById(R.id.deptno);
+        desg=(EditText) findViewById(R.id.desg);
+        email=(EditText) findViewById(R.id.email);
+        contact=(EditText) findViewById(R.id.phno);
+        address=(EditText) findViewById(R.id.address);
+        pass=(EditText) findViewById(R.id.password);
+        noleaves=(EditText) findViewById(R.id.leaves);
+        register=(Button) findViewById(R.id.register);
+
+
     }
 }
--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -2,18 +2,34 @@ package com.example.android;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     Button nwuser;
     Button login;
     EditText eid;
+    TextView tevi;
+    String URL = "https://cosc-team-14-restapi.herokuapp.com/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eid = (EditText) findViewById(R.id.empid);
+        tevi = (TextView) findViewById(R.id.textView9);
         nwuser = (Button) findViewById(R.id.newuser);
         nwuser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,20 +37,16 @@ public class MainActivity extends AppCompatActivity {
                 newuser();
             }
         });
-     /*   login = (Button) findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });*/
+        login = (Button) findViewById(R.id.login);
+
     }
     public void newuser(){
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
     }
-  /*  public void login(){
-        Intent intent = new Intent(this,firstpg.class);
-        startActivity(intent);
-    } */
+    public void login(){
+
+            Intent intent = new Intent(this, firstpg.class);
+            startActivity(intent);
+    }
 }
