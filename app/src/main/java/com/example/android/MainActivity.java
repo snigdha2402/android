fcff9214acde0d3599f58d0ccfcd4d534246610d
snigdha2402 <suramsnigdha@gmail.com>
2020-07-03 17:00:15 +0530

    commit

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -2,39 +2,44 @@ package com.example.android;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    JsonObjectRequest objectRequest;
+    JSONObject j;
     TextView nwuser;
     Button login;
     EditText eid,pass;
-
-    TextView del;
-
     String empid;
     String passwrd;
-    String URL = "https://cosc-team-14-restapi.herokuapp.com/emp_register";
+    String URL = "https://cosc-team-14-restapi.herokuapp.com/emp_login";
     RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         eid = (EditText) findViewById(R.id.empid);
         pass=(EditText) findViewById(R.id.password);
         nwuser = (TextView) findViewById(R.id.newuser);
@@ -47,12 +52,18 @@ public class MainActivity extends AppCompatActivity {
         login = (Button) findViewById(R.id.login);
 
 
-
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 login();
 
+                if( eid.getText().toString().equals("1234") &&  pass.getText().toString().equals("pass"))
+                { login();}
+              /*  try {
+                    login();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                queue.add(objectRequest); */
             }
         });
 
@@ -62,8 +73,41 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Main2Activity.class);
         startActivity(intent);
     }
-    public void login() {
+    public void login()  {
          Intent intent = new Intent(this, firstpg.class);
          startActivity(intent);
+     /*   j = new JSONObject();
+            j.put("EMPID", eid.getText().toString());
+            j.put("PASS", pass.getText().toString());
+
+        queue = Volley.newRequestQueue(this);
+
+
+        objectRequest = new JsonObjectRequest(Request.Method.POST,
+                URL,
+                j,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("rest response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("rest response", error.toString());
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json");
+                String creds = String.format("%s:%s", "username", "password");
+                String auth = "Basic " + Base64.encodeToString(creds.getBytes(), Base64.DEFAULT);
+                params.put("Authorization", auth);
+                return params;
+            }
+        }; */
+
     }
 }
